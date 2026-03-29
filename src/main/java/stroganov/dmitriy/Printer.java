3f50200355c08abd71ac54cbcf988d4d853a9017
dstroganov <dstroganov@diasoft.ru>
2026-03-29 17:53:59 +0700

    синхронизированный метод

--- src/main/java/stroganov/dmitriy/Printer.java
+++ src/main/java/stroganov/dmitriy/Printer.java
@@ -2,7 +2,7 @@ package stroganov.dmitriy;
 
 public class Printer {
 
-    public void print(String threadName) throws InterruptedException {
+    public synchronized void print(String threadName) throws InterruptedException {
         System.out.println("поток " + threadName + " начал работу");
         Thread.sleep(1000);
         System.out.println("поток " + threadName + " продолжает работу");
