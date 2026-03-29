ed9bc56200fa5834774728c6adf21721b7928537
dstroganov <dstroganov@diasoft.ru>
2026-03-29 17:52:11 +0700

    новые файлы

--- src/main/java/stroganov/dmitriy/Printer.java
+++ src/main/java/stroganov/dmitriy/Printer.java
@@ -1,4 +1,14 @@
 package stroganov.dmitriy;
 
 public class Printer {
+
+    public void print(String threadName) throws InterruptedException {
+        System.out.println("поток " + threadName + " начал работу");
+        Thread.sleep(1000);
+        System.out.println("поток " + threadName + " продолжает работу");
+        Thread.sleep(1000);
+        System.out.println("поток " + threadName + " почти закончил работу");
+        Thread.sleep(1000);
+        System.out.println("поток " + threadName + " закончил работу");
+    }
 }
--- src/main/java/stroganov/dmitriy/PrinterThread.java
+++ src/main/java/stroganov/dmitriy/PrinterThread.java
@@ -1,4 +1,19 @@
 package stroganov.dmitriy;
 
-public class priterThread {
+public class PrinterThread extends Thread{
+    private final Printer printer;
+
+    public PrinterThread(String name, Printer printer) {
+        super(name);
+        this.printer = printer;
+    }
+
+    @Override
+    public void run() {
+        try {
+            printer.print(this.getName());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
