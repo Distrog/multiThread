964e601d52491963c301e3b91b4209ce4a65589d
dstroganov <dstroganov@diasoft.ru>
2026-03-29 18:08:09 +0700

    использование wait и notify

--- src/main/java/stroganov/dmitriy/Printer.java
+++ src/main/java/stroganov/dmitriy/Printer.java
@@ -2,13 +2,27 @@ package stroganov.dmitriy;
 
 public class Printer {
 
-    public void print(String threadName) throws InterruptedException {
-        System.out.println("поток " + threadName + " начал работу");
-        Thread.sleep(1000);
-        System.out.println("поток " + threadName + " продолжает работу");
-        Thread.sleep(1000);
-        System.out.println("поток " + threadName + " почти закончил работу");
-        Thread.sleep(1000);
-        System.out.println("поток " + threadName + " закончил работу");
+    private boolean isFree = true;
+
+    public synchronized void print(String threadName) throws InterruptedException {
+
+        while (!isFree) {
+            wait();
+        }
+
+        isFree = false;
+
+        try {
+            System.out.println("поток " + threadName + " начал работу");
+            Thread.sleep(1000);
+            System.out.println("поток " + threadName + " продолжает работу");
+            Thread.sleep(1000);
+            System.out.println("поток " + threadName + " почти закончил работу");
+            Thread.sleep(1000);
+            System.out.println("поток " + threadName + " закончил работу");
+        } finally {
+            isFree = true;
+            notifyAll();
+        }
     }
 }
